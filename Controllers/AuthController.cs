0bc36cd72ad48c7e556b4b6decef631509ba5d41
f.asteur <asteur.florian@gmail.com>
2020-08-05 16:09:00 +0200

    feat: add login + authentication (DOTNET-3)

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using DOTNET_RPG.Data;
+using DOTNET_RPG.Dtos.UserRegister;
+using DOTNET_RPG.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DOTNET_RPG.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+
+        public AuthController(IAuthRepository authRepository)
+        {
+            _authRepo = authRepository;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(UserRegisterDto request)
+        {
+            ServiceResponse<int> response = await _authRepo.Register(
+            new User { Username = request.Username }, request.Password);
+
+            if(!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserLoginDto request)
+        {
+            ServiceResponse<string> response = await _authRepo.Login(request.Username, request.Password);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
